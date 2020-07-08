16359e3812b3677ad2581958a0d0c17200b65382
German Castro <germancastromontes@gmail.com>
2020-07-08 20:28:47 +0200

    alumn updates

--- src/schemas/alumn.js
+++ src/schemas/alumn.js
@@ -7,14 +7,14 @@ const typeDefs = gql`
     name: String!
     surname: String!
     email: String!
-    seminar: Seminar!
+    seminar: Seminar
   }
 
   type Seminar {
     id: ID!
     title: String!
     description: String!
-    alumn: Alumn!
+    alumn: Alumn
   }
 
   extend type Query {
@@ -24,11 +24,15 @@ const typeDefs = gql`
   }
 `;
 
+
+const include = {
+  seminar: true,
+}
 const resolvers = {
   Query: {
-    alumns: (parent, { take, skip }, { prisma }) => prisma.alumn.findMany({ skip, take }),
-    alumnById: (parent, { id }, { prisma }) => prisma.alumn.findOne({ where: { id } }),
-    alumnByEmail: (parent, { email }, { prisma }) => prisma.alumn.findOne({ where: { email } }),
+    alumns: (parent, { take, skip }, { prisma }) => prisma.alumn.findMany({ skip, take, include }),
+    alumnById: (parent, { id }, { prisma }) => prisma.alumn.findOne({ where: { id }, include }),
+    alumnByEmail: (parent, { email }, { prisma }) => prisma.alumn.findOne({ where: { email }, include }),
   }
 };
 
